d3c8f038c85c2674c1e2a0973a1bbd952cba555d
Mani Parhizkar <maniparhizkar2017@gmail.com>
2022-03-13 22:10:53 +0330

    flex-end

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -24,12 +24,6 @@ const Courses = () => {
                 <h3> دوره ها</h3>
             </div>
             <div className={styles.courseContainer}>
-                {/* <Course image={sass} name="آموزش SASS" students="3,933" price="149000– رایگان!"/>
-                <Course image={react} name="دوره جامع متخصص ریکت و ریداکس" students="831" price="2,289,000"/>
-                <Course image={git} name="دوره آموزش گیت، گیت‌هاب و گیت‌لب" students="3,933" price="219,000"/>
-                <Course image={javaScript} name="آموزش mobile first design" students="402" price="رایگان!"/>
-                <Course image={html} name="دوره جامع html و css" students="1,023" price="349,000 "/>
-                <Course image={mobile} name="دوره مقدماتی تا پیشرفته جاوااسکریپت" students="1,111" price="1,329,000"/> */}
                 {products.map(product => <Course key={product.name} image={product.image} name={product.name} url={product.url} students={product.students} price={product.price}/>)}
             </div>
         </div>
